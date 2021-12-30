bf699cc58fda39353149097b82a4a5affd2dd0d7
Ami <amiweil2@gmail.com>
2021-12-30 23:38:38 +0200

    code cleanup. file naming changes. fixes.

--- data_visualizations.py
+++ data_visualizations.py
@@ -3,10 +3,6 @@ import pickle
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mutual_info_score as mmi
-from sklearn.metrics import normalized_mutual_info_score as nmi
-from sklearn.metrics import adjusted_mutual_info_score as ami
-from sklearn.metrics import v_measure_score as vms
 from sklearn.metrics.pairwise import cosine_distances, cosine_similarity, pairwise_distances
 from scipy.stats import pearsonr as prs, spearmanr
 import lang2vec.lang2vec as l2v
@@ -30,11 +26,12 @@ def rank_correlation(errors_dist, lang2vec_dist, langs):
     plt.bar(lang_labels, corrs)
     plt.title('spearman rank correlation of distances of language from other '
               'languages \n by error-profile vs. lang2vec profile')
-    pv_str = [f'p value=\n{pv}' for pv in pvs]
+    pv_str = [f'p value=\n{pv:.4f}' for pv in pvs]
     for i in range(len(corrs)):
         plt.annotate(pv_str[i], (i, 0.2), ha='center')
     plt.gcf().set_size_inches(25, 16, forward=False)
     plt.savefig("graphs/rank_correlation.png", dpi=80)
+    plt.close()
 
 
 
@@ -106,6 +103,9 @@ def allDistFromLang(all_heats, langs):
     allDist = allDist.set_index('from')
     title = 'Distances between languages, by error profiles. \n computed by cosine similarity.'
     saveHeatMap(allDist, title, fileName="graphs/distances_prs.png")
+    plt.bar(allDist.sum().sort_values().index, allDist.sum().sort_values().values)
+    plt.yscale('log')
+    save_close('dist_error_summed', 'Sum of distances by language (error-profile)', 'language', 'sum of distances (log scale)')
     return allDist
 
 
@@ -120,7 +120,7 @@ def saveHeatMap(mat, title='', x='', y='', fileName='temp.png'):
     plt.ylabel(y)
     plt.xlabel(x)
     fig.set_size_inches(22, 12, forward=False)
-    plt.savefig(fileName, dpi=80)
+    plt.savefig(fileName.replace(' ', '_').replace(':', ''), dpi=80)
     plt.close()
 
 
@@ -147,6 +147,8 @@ def allDistLang2vec(langs):
     distL2vCS = distL2vCS.set_index('from')
     title = 'Distances between languages, by Lang2Vec. \n computed by cosine similarity.'
     saveHeatMap(distL2vCS, title, fileName="graphs/distances_prs_l2v.png")
+    plt.bar(distL2vCS.sum().sort_values().index, distL2vCS.sum().sort_values().values)
+    save_close('dist_lang2vec_summed', 'Sum of distances by language', 'language', 'sum of distances')
     return distL2vCS
 
 
@@ -166,8 +168,6 @@ def print_to_log(*text, new_path=None):
 
 
 def compare_all_dimitry(df):
-    # TODO: make image smaller, so font is bigger.
-    # TODO: in compare_dimitry, give all_heats instead of df. Will cut runtime... (Who cares?..)
     cds = []
     for lang in ['ru', 'fr', 'jp', 'cn'] :
         for d_lang in ['ru', 'fr', 'jp', 'zh'] :
@@ -242,13 +242,13 @@ def save_heat(heat, lang):
     ax = sns.heatmap(heat, annot=True, fmt=fmt, cmap='Blues', linewidths=0.3, cbar_kws={'shrink': 0.8}, square=False)
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom + 0.5, top - 0.5)
-    plt.title(" Syntactic relations English - "+lang+"\n")
+    plt.title(f'Syntactic relations English - {lang} ')
     fig = plt.gcf()
     plt.ylabel('English')
     plt.xlabel(lang)
     fig.set_size_inches(22, 12, forward=False)
     # plt.tight_layout()
-    plt.savefig("graphs/heat_" + lang + ".png".replace(' ', '_'), dpi=80)
+    plt.savefig(f'graphs/heat_{lang}.png'.replace(' ', '_').replace(':', ''), dpi=80)
     plt.close()
 
 
@@ -279,7 +279,7 @@ def save_err_profile_minus_avg_by_nationality(df, languages):
     avg_mat['pre'] = avg_mat['err'].apply(lambda x: x.strip().split('->')[0])
     avg_mat['post'] = avg_mat['err'].apply(lambda x: x.strip().split('->')[1])
     heat = avg_mat.pivot(index='pre', columns='post', values='mat').fillna(0)
-    save_heat(heat, ' avg')
+    save_heat(heat, ' avg by nationality')
 
     # every language minus the average:
     for lang in languages:
@@ -332,7 +332,7 @@ def per_level_bar(df, levels):
     :param levels: Which levels to include in graph (list of numbers. ) order is important
     :return:
     """
-    # TODO: create multi graph plt, in each a bar plot of all levels for that error. manually.
+    #  create multi graph plt, in each a bar plot of all levels for that error. manually.
     levels.sort()
     first = levels[0]
     all_mats = get_mat_vals(df[df[COL_LEVEL] == first])
@@ -437,47 +437,44 @@ if __name__ == '__main__':
     print_to_log('starting')
     pkl = 'total/total.pkl'
     df = pickle.load(open(pkl, 'rb'))
-    df=df[df[COL_N_ERROR]!='R:OTHER'] # found one 'other'
+    df=df[df[COL_N_ERROR]!='R:OTHER'] # found only one 'R:OTHER'
+    df=df[df[COL_LEVEL]!='4']  # small DS
+    df=df[df[COL_LEVEL]!='6']  # small DS
     print_to_log('loaded')
     df = df.drop(columns=['orig'])
-    freq_errors = get_freq(df, COL_N_ERROR)
-    print_to_log('got freq errors')
-    freq_langs = get_freq(df, COL_LANG)
-    print_to_log('got freq languages')
-    freq_levels = get_freq(df, COL_LEVEL)
-    # levels = freq_levels[COL_LEVEL].to_list()[:-2]
-    levels = freq_levels[COL_LEVEL].to_list()[:13]
-    langs = freq_langs[COL_LANG].to_list()[:10]
-    freq_err = freq_errors[COL_N_ERROR].to_list()[:20]
+    freq_errors, freq_langs, freq_levels = dataset_statistics(df)
+    levels = freq_levels[COL_LEVEL].to_list()
+    N_LANGUAGES = 20 # Number of languages to use for calculations
+    all_languages = freq_langs[COL_LANG].to_list()[:N_LANGUAGES]
+    # freq_err = freq_errors[COL_N_ERROR].to_list()[:20]
+    df=df[df[COL_LANG].isin(all_languages)]
     # print(freq_langs)
     # print(freq_errors)
-
-    total_heat = get_heat(df)
-    save_heat(total_heat, 'test tight')
-    print_to_log('got total heat')
-    N = 3  # Number of languages to use (N largest in dataset. discard the smaller ones.)
-    languages = freq_langs[COL_LANG].to_list()[:N]
-    save_err_profile_by_nationality(df, languages)
-    save_err_profile_minus_avg_by_nationality(df, languages)
-
+    N = 10  # Number of languages to use for graphs (N largest in dataset. discard the smaller ones.)
+    langs = all_languages[:N]
+    vis_whole_ds(df)
+    save_err_profile_by_nationality(df, langs)
+    save_err_profile_minus_avg_by_nationality(df, langs)
+    save_err_profile_minus_avg_by_level(df, levels)
+    save_err_profile_by_nationality_level(df, langs, levels)
+    save_err_profile_by_level(df, levels)
     per_level_bar(df, levels) # the real one..!
-    dimitry_path = 'external_data/en-zh_pos_cm_percent.csv'
-    lang = 'cn'
-    r = compare_dimitry(df, 'external_data/en-ru_pos_cm_percent.csv', 'ru')
-    f = compare_dimitry(df, 'external_data/en-fr_pos_cm_percent.csv', 'fr')
-    j = compare_dimitry(df, 'external_data/en-jp_pos_cm_percent.csv', 'jp')
-    c = compare_dimitry(df, 'external_data/en-zh_pos_cm_percent.csv', 'cn')
-    # compare_dimitry(df, 'external_data/en-ko_pos_cm_percent.csv', 'ko')
-    print(r,f,j,c)
 
     all_heats = dict()
-    for lang in langs:
+    for lang in all_languages:
         if lang in country_to_lang:
             all_heats[lang] = get_heat(df[df[COL_LANG] == lang])
 
-    errors_dist = allDistFromLang(all_heats, langs)
-    lang2vec_dist = allDistLang2vec(langs)  # objective no. 1 . complete!
-    # TODO: rank spearman (objective 2). one number? what is the output??
-    rank_correlation(errors_dist, lang2vec_dist, langs)
+    errors_dist = allDistFromLang(all_heats, all_languages)
+    lang2vec_dist = allDistLang2vec(all_languages)  # objective no. 1 . complete!
+    rank_correlation(errors_dist, lang2vec_dist, all_languages)
+
+    df['pre'] = df[COL_N_ERROR].apply(lambda x: x.strip().split('->')[0])
+    df['post'] = df[COL_N_ERROR].apply(lambda x: x.strip().split('->')[1])
+    vis_words(df)
+    vis_words(df, 'post', 'SYM')
+
+    compare_all_dimitry(df)
+
 
 
